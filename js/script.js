bf3630c303bb383c2a957ea73ef26d5750eb50f5
Miguel Alves Pires De Oliveira <miguel0439@DIGITALCOLLEGE.LOCAL>
2024-06-20 19:30:52 -0300

    Inicio do projeto exibindo a hora

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function mostrarDataHora() {
+    const date = new Date();
+    console.log(date.toLocaleString())
+
+    document.getElementById('date').innerText = date.toLocaleString();
+}
+
+mostrarDataHora();
